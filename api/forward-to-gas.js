4b71b59f3d5fabac50a99ff0ebd27259e8de34ef
Mikey <mark.weerasinghe@onramper.com>
2025-07-06 13:07:21 +0530

    Update and rename forward-to-hubspot.js to forward-to-gas.js

---
+++ api/forward-to-gas.js
@@ -0,0 +1,25 @@
+export default async function handler(req, res) {
+  const payload = req.body;
+
+  try {
+    const gasResponse = await fetch("https://script.google.com/macros/s/AKfycbx_io7IFLs3RfW09-9wMuN8SsSfXbjwrCJekpatOZwQEtzaAG6784FtZJOjK7xY-OfI0Q/exec", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(payload)
+    });
+
+    if (!gasResponse.ok) {
+      const errorText = await gasResponse.text();
+      console.error("GAS Email Failed:", errorText);
+      return res.status(500).json({ error: "Failed to send email via Google Apps Script" });
+    }
+
+    return res.status(200).json({ message: "Email sent via GAS successfully" });
+
+  } catch (err) {
+    console.error("Fetch Error:", err);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+}
